94513d2ddf846b6a6743c5fb325178abd427b72c
Akshay Mohan <145959795+akshmohan@users.noreply.github.com>
2024-02-06 17:09:31 +0530

    First commit

---
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.viewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var textView : TextView
+    lateinit var button: Button
+    lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        textView = findViewById(R.id.text_view)
+        button = findViewById(R.id.btn)
+
+        textView.text = "Count is ${viewModel.count}"
+
+        button.setOnClickListener{
+            viewModel.increaseCount()
+            textView.text = "Count is ${viewModel.count}"
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/viewmodel/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    var count = 0
+
+    fun  increaseCount() {
+        count++
+    }
+
+}
